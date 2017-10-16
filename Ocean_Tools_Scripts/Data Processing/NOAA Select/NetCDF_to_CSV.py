5ab87ad7c6122a0725a8a068dce346e93a76cced
Keith VanGraafeiland <keit8223@esri.com>
2017-10-16 10:26:56 -0700

    Updated processing Script for Bulk Processing into a single file

--- Ocean_Tools_Scripts/Data Processing/NOAA Select/NetCDF_to_CSV.py
+++ Ocean_Tools_Scripts/Data Processing/NOAA Select/NetCDF_to_CSV.py
@@ -5,6 +5,7 @@
 #
 # Author:       Keith VanGraafeiland
 # Created:      July 7, 2017
+# Modified:     October 15, 2017
 # Copyright:    (c) Keith VanGraafeiland 2017
 # Licence:      MIT License
 # Version:      0.1
@@ -23,24 +24,30 @@ import csv
 directory = arcpy.GetParameterAsText(0)
 varName = arcpy.GetParameterAsText(1)
 
+def getOutputCsvFilePath():
+    out_file = varName + '.txt'
+    outputFilePath = os.path.join(directory, out_file)
+    return outputFilePath
+
 ##function
-def processItmesInMainDir(dirName):
+def processItemsInMainDir(index, dirName, outputCsvFile):
 
     lastPartOfFolderName = dirName.split('.')[2]
 
-    out_file = varName + "_" + lastPartOfFolderName + '.txt'
+##    out_file = varName + "_" + lastPartOfFolderName + '.txt'
+##    outputFilePath = os.path.join(directory, out_file)
+##    outputFilePath = outputCsvFilePath
+##    arcpy.AddMessage("outputFilePath: " + outputFilePath)
+##    out = open(outputFilePath, 'w')
 
-    outputFilePath = os.path.join(directory, out_file)
+    writer = csv.writer(outputCsvFile)
 
-    arcpy.AddMessage("outputFilePath: " + outputFilePath)
-
-    out = open(outputFilePath, 'w')
-    writer = csv.writer(out)
-    writer.writerow(('date', 'lat', 'lon','z', varName))
+    if(index == 0):
+        writer.writerow(('date', 'lat', 'lon','z', varName,'directory','filename'))
 
     proceeItemsInSubDir(dirName, writer)
 
-    out.close()
+##    out.close()
 
     arcpy.AddMessage("Done Processing " + varName)
 
@@ -49,22 +56,32 @@ def proceeItemsInSubDir(dirName, writer):
     dirName = os.path.join(directory, dirName)
 
     for filename in os.listdir(dirName):
+
         if filename.endswith('.nc'):
             filepath = os.path.join(dirName, filename)
+            fileFolderParts = dirName.split("\\")
+            fileFolderName = fileFolderParts[len(fileFolderParts) - 1]
             print(filepath)
             f = netCDF4.Dataset(filepath,'r')
             value_array = f.variables[varName][:]
+
             for i in range(0, len(value_array)):
                 if f.variables[varName][i] > 0:
-                    writer.writerow((f.variables['date'][0], f.variables['lat'][0], f.variables['lon'][0], f.variables['z'][i], '{:.20f}'.format(f.variables[varName][i])))
+                    writer.writerow((f.variables['date'][0], f.variables['lat'][0], f.variables['lon'][0], f.variables['z'][i], '{:.20f}'.format(f.variables[varName][i]),fileFolderName,filename))
             f.close()
+
         else:
             continue
 
 def executeScript():
-    for name in os.listdir(directory):
+    outputCsvPath = getOutputCsvFilePath()
+    outputCsvFile = open(outputCsvPath, 'w')
+
+    for idx, name in enumerate(os.listdir(directory)):
         if os.path.isdir(os.path.join(directory, name)):
-            processItmesInMainDir(name)
+            processItemsInMainDir(idx, name, outputCsvFile)
+
+    outputCsvFile.close()
 
 ## start executing the script
 executeScript()
